df55561da887074833f9457b0fceb8260b447439
Alix ANNERAUD <alix.anneraud@outlook.fr>
2023-10-13 13:12:15 +0200

    Update main and add http server async

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -3,16 +3,43 @@
 #include "pin.hpp"
 #include "lcd.hpp"
 
+#include <thread>
+
+#include "httplib.h"
+
+void executeServer()
+{
+    httplib::Server svr;
+
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res)
+            { res.set_content("Hello World!", "text/plain"); });
+
+    svr.listen("0.0.0.0", 8080);
+}
+
 int main()
 {
     if (!PinClass::initialize())
     {
         LOG_ERROR("Main", "Failed to initialize pin class.");
-        return 1;
+        return EXIT_FAILURE;
     }
 
+    LOG_INFORMATION("Main", "Initialized pin class.");
+
+    std::thread(executeServer).detach();
+
+    PinClass Pin(2);
+
+    Pin.setMode(PinMode::Output);
 
     LOG_INFORMATION("Main", "Starting program.");
 
-    return 0;
+    while (true)
+    {
+        
+    }
+    
+
+    return EXIT_SUCCESS;
 }
