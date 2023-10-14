588c36fb254659099b1e00d250de61082f50ef32
DimitriTimoz <dimitri.timoz@orange.fr>
2023-10-14 20:02:41 +0200

    maybe pid works

--- code/include/pid.hpp
+++ code/include/pid.hpp
@@ -20,7 +20,8 @@ private:
     long double tau;
     
     // time step
-    clock_t dt[3] = {0, 0, 0};
+    long double dt[3] = {0, 0, 0};
+    clock_t lastRecordClock = 0;
     
 
 public:
@@ -28,11 +29,41 @@ public:
     PidControlClass(long double Kp, long double Ki, long double Kd, long double setpoint);
     ~PidControlClass();
 
-    double getSteeringAngle(double measuredValue, clock_t recordClock);
+    long double getSteeringAngle(double measuredValue, clock_t recordClock);
 };
 
 
-
-
+/* Pseudocode
+A0 := Kp + Ki*dt
+A1 := -Kp
+error[2] := 0 // e(t-2)
+error[1] := 0 // e(t-1)
+error[0] := 0 // e(t)
+output := u0  // Usually the current value of the actuator
+A0d = Kd/dt
+A1d = - 2.0*Kd/dt
+A2d = Kd/dt
+N := 5
+tau := Kd / (Kp*N) // IIR filter time constant
+alpha = dt / (2*tau)
+d0 := 0
+d1 := 0
+fd0 := 0
+fd1 := 0
+loop:
+    error[2] := error[1]
+    error[1] := error[0]
+    error[0] := setpoint − measured_value
+    // PI
+    output := output + A0 * error[0] + A1 * error[1]
+    // Filtered D
+    d1 = d0
+    d0 = A0d * error[0] + A1d * error[1] + A2d * error[2]
+    fd1 = fd0
+    fd0 = ((alpha) / (alpha + 1)) * (d0 + d1) - ((alpha - 1) / (alpha + 1)) * fd1
+    output := output + fd0      
+    wait(dt)
+    goto loop
+*/
 
 #endif // !PID_CONTROL
--- code/src/pid.cpp
+++ code/src/pid.cpp
@@ -6,19 +6,34 @@ PidControlClass::PidControlClass(long double Kp, long double Ki, long double Kd,
     : Kp(Kp), Ki(Ki), Kd(Kd), setpoint(setpoint) 
 {
     tau = Kd / (Kp * PID_N);
-
+    A1 = -Kp;
 }
 
-double PidControlClass::getSteeringAngle(double measuredValue, clock_t recordClock) {
-    records[2] = records[1];
-    records[1] = records[0];
-    records[0] = recordClock;
+long double PidControlClass::getSteeringAngle(double measuredValue, clock_t recordClock) {
+    dt[2] = dt[1];
+    dt[1] = dt[0];
+    dt[0] = (long double) ((recordClock - lastRecordClock) / CLOCKS_PER_SEC);
+    lastRecordClock = recordClock;
 
     errors[2] = errors[1];
     errors[1] = errors[0];
-    errors[0] = setpoint - measuredValue;
+    errors[0] = setpoint - (long double)measuredValue;
 
+    // Adapt to the shift of the time steps
+    A0 = Kp + Ki * dt[0];
+    A0d = Kd / dt[0];
+    A1d = -2.0 * Kd / dt[1];
+    A2d = Kd / dt[2];
+    alpha = dt[0] / (2 * tau);
     // Propotional integral terms
     output += A0 * errors[0] + A1 * errors[1];
-    return 0.0;
+
+    // Filtered derivative term
+    d1 = d0;
+    d0 = A0d * errors[0] + A1d * errors[1] + A2d * errors[2];
+    fd1 = fd0;
+    fd0 = ((alpha) / (alpha + 1)) * (d0 + d1) - ((alpha - 1) / (alpha + 1)) * fd1;
+    output += fd0;
+
+    return output;
 }
