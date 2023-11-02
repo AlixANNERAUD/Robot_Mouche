63ef43e5d9e29f9514974f03ef21cd84075b4548
Alix ANNERAUD <alix@anneraud.fr>
2023-11-03 00:42:46 +0100

    Move server to core library

---
+++ code/lib/Core/src/server.cpp
@@ -0,0 +1,77 @@
+#include "server.hpp"
+#include "log.hpp"
+#include "httplib.h"
+
+ServerClass::ServerClass() {
+    this->on_gamepad_direction = [](float, float) {
+        LOG_WARNING("Server", "Unhandled gamepad direction!");
+    };
+    this->on_settings_change = [](SettingsClass) {
+        LOG_WARNING("Server", "Unhandled settings change!");
+    };
+}
+
+void ServerClass::listen() {
+    httplib::Server server;
+
+    LOG_INFORMATION("Server", "Starting server");
+
+    GamepadDirectionCallback *on_gamepad_direction = this->on_gamepad_direction;
+    SettingsChangeCallback *on_settings_change = this->on_settings_change;
+
+    server.Post("/gamepad-direction", [on_gamepad_direction](const httplib::Request &req, httplib::Response &res)
+    { 
+        // Ensure body is long enough
+        if (req.body.length() < 8) {
+            LOG_ERROR("Server", "Gamepad direction request body too short!");
+            res.set_content("Gamepad direction request body too short!", "text/plain"); 
+            return;
+        }
+
+        // Read game pad direction from body which is 4 bytes for x and 4 bytes for y as LE
+        float x = *(float *)req.body.substr(0, 4).c_str();
+        float y = *(float *)req.body.substr(4, 4).c_str();
+        LOG_DEBUG("Server", "Gamepad direction received: %f %f", x, y);
+
+        // Call callback
+        on_gamepad_direction(x, y);
+
+        // Respond
+        res.set_content("All good!", "text/plain"); 
+    });
+
+    server.Post("/settings", [on_settings_change](const httplib::Request &req, httplib::Response &res)
+    { 
+        // Body format: 1 byte for mode (0 = line follower, 1 = manual)
+
+        if (req.body.length() != 1) {
+            LOG_ERROR("Server", "Settings request body wrong length!");
+            res.set_content("Settings request body wrong length!", "text/plain");
+            return;
+        }
+
+        // Read body
+        RobotMode mode;
+        switch (req.body[0]) {
+            case 0:
+                LOG_DEBUG("Server", "Settings request received: Line follower");
+                mode = RobotMode::LineFollower;
+                break;
+            case 1:
+                LOG_DEBUG("Server", "Settings request received: Manual");
+                mode = RobotMode::Manual;
+                break;
+            default:
+                LOG_ERROR("Server", "Settings request received: Unknown mode");
+                res.set_content("Settings request received: Unknown mode", "text/plain");
+                return;
+        }
+
+        // Respond
+        LOG_INFORMATION("Server", "Settings request received: All good!");
+        on_settings_change(SettingsClass(mode));
+        res.set_content("All good!", "text/plain"); 
+    });
+
+    server.listen("0.0.0.0", 8080);
+}
