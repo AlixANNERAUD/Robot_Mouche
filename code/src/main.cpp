d3c64330a415413bb6c5941a7452913e4c22643c
Alix ANNERAUD <alix@anneraud.fr>
2024-01-07 22:41:09 +0100

    Update main

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -17,21 +17,25 @@
 
 int main()
 {
+    LOG_INFORMATION("Main", "Starting program.");
+
     if (!PinClass::initialize())
     {
         LOG_ERROR("Main", "Failed to initialize pin class.");
         return EXIT_FAILURE;
     }
+    LOG_INFORMATION("Main", "Initialized pin class.");
 
     PinClass I2C_SDA(2), I2C_SCL(3);
-
-    // display(I2C_SDA, I2C_SCL);
-
-    LOG_INFORMATION("Main", "Initialized pin class.");
 
     LiDARClass lidar(I2C_SDA, I2C_SCL);
     if (!lidar.isValid())
         return EXIT_FAILURE;
+
+    LCDClass lcd(I2C_SDA, I2C_SCL);
+    if (!lcd.isValid()) {
+        return EXIT_FAILURE;
+    }
 
     PinClass rightMotorEnabled(13);
     PinClass rightMotorA1(27);
@@ -44,17 +48,15 @@ int main()
     MotorClass rightMotor(rightMotorEnabled, rightMotorA1, rightMotorA2);
     MotorClass leftMotor(leftMotorEnabled, leftMotorA1, leftMotorA2);
 
-    LOG_INFORMATION("Main", "Starting program.");
-
-    DriverClass driver(lidar, leftMotor, rightMotor);
+    DriverClass driver(lidar, leftMotor, rightMotor, lcd);
 
     server(driver);
-
     driver.start();
+
     while (true)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
-    //    driver.stop();
+    driver.stop();
     return EXIT_SUCCESS;
 }
